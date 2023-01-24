2c7f4183aed57f62aae8217dbab37c3585ea64b9
vuchienthang <thangveo.cp99@gmail.com>
2023-01-24 08:44:55 +0300

    Initial Commit

---
+++ Clima SwiftUI/Clima_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clima_SwiftUIApp.swift
+//  Clima SwiftUI
+//
+//  Created by Vũ Chiến Thắng on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Clima_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
